6fd76322d5a9c58f3d0280a7ea31628101280830
Saikaze <saikaze@gmail.com>
2015-12-31 15:09:25 +0000

    v1.0.0

--- helper.class.php
+++ helper.class.php
@@ -5,3 +5,62 @@
  * Date: 31/12/2015
  * Time: 15:00
  */
+
+
+class helper{
+
+    /**
+     *
+     * @param string $sku
+     * @param bool $returnArray
+     * @return mixed
+     */
+    public static function returnPrefix($sku, $returnArray = false)
+    {
+        $skuParts = explode("-", $sku);
+        $prefix = '';
+        $category = $skuParts[count($skuParts) - 2];
+        $design = $skuParts[count($skuParts) - 1];
+        for ($i = 0; $i < count($skuParts) - 2; $i ++){
+            $prefix .= $skuParts[$i] . "-";
+        }
+        $prefix = substr($prefix, 0, strlen($prefix) - 1 );
+        if ($returnArray)
+            return $prefix;
+        else
+            return array($prefix, $category, $design);
+    }
+
+
+    /**
+     * @param mixed $to
+     * @param string $subject
+     * @param string $body
+     * @return bool
+     * @throws phpmailerException
+     */
+    public static function sendSimpleMail($to, $subject, $body)
+    {
+
+        $mail = new PHPMailer;
+        $mail->setFrom('it@virano.com', 'Saikaze/Helper');
+
+        if (is_array($to))
+            foreach($to as $address)
+                $mail->addAddress($address);
+        else
+            $mail->addAddress($to);
+
+        $mail->addReplyTo('it@virano.com');
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+
+        if(!$mail->send())
+            return false;
+        else
+            return true;
+    }
+
+
+}
